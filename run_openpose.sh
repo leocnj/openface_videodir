34e379545d7434e5e32ce41e373cb9915960f44b
Lei Chen <lei.chen@liulishuo.com>
2018-06-06 15:33:52 -0400

    added run_openpose.sh to do pose, hand, and face landmark tracking by using OpenPose

---
+++ run_openpose.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#
+one_video() {
+  video=$(basename $1 .mp4)
+  echo $video
+  docker exec openpose ./build/examples/openpose/openpose.bin \
+  --video /data/${video}.mp4\
+  --write_video /data/op_videos/${video}.avi\
+  --write_keypoint_json /data/op_poses\
+  --face --hand --no_display
+}
+# https://unix.stackexchange.com/questions/271134/how-to-make-a-function-available-to-the-command-parallel-gnu
+export -f one_video
+
+data_dir=/workspace/lchen/works/data/engagement/fps10
+root=$1    # train vs validation
+# run docker in a daemon mode
+docker run --runtime=nvidia --name openpose --rm -v ${data_dir}/${root}:/data -d -it garyfeng/docker-openpose:latest
+cd ${data_dir}/${root}
+ls *.mp4 > videos.tmp
+parallel -j 1 one_video < videos.tmp
+rm -f videos.tmp
+docker stop -openpose
